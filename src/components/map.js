33b46543d2a3d5ebc18d2b104d6e1ff5eb72fa12
Rodolfo Hernandez <rodolfo.rahp@gmail.com>
2017-10-23 00:09:44 +0000

    Adding static server

--- src/components/map.js
+++ src/components/map.js
@@ -5,8 +5,8 @@ import Marker from './marker';
 
 const Map = compose(
   withProps({
-    googleMapURL:
-    "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: // TODO: create env to get keys
+    "https://maps.googleapis.com/maps/api/js?key=AIzaSyDzPKnIN2acJ9S8GYhCYsiJxAardfs0erE&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `700px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
